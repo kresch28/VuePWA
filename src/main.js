45ca209ea5e0c6b4af20cbf867a8d5fc018a22f0
Kathi <kathi.resch28@gmail.com>
2020-03-30 09:45:05 +0200

    PWA Setup

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {faMinus, faUserSecret, faPlus} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import './registerServiceWorker'
 
 library.add(faUserSecret);
 library.add(faPlus);
